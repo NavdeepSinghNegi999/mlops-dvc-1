2ebb264b961435c0d7ba53fa4f98d35d8b2f5a4d
navdeep <navdeepsinghnegi999@gmail.com>
2025-10-30 07:35:29 +0000

    1-initial-commit

---
+++ demo.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import os 
+
+# PATHS
+ROOT_DIR = "mlops-dvc-1"
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+
+# Create data frame.
+data = {"name":["name1", "name2", "name3"],
+    "age":[10,20,30]
+}
+df = pd.DataFrame(data)
+
+
+# Check if data folder exists else create it.
+os.makedirs(DATA_DIR, exist_ok=True)
+
+
+# Save the dataframe
+df.to_csv(os.path.join(DATA_DIR, "info.csv"), index=False)
+print(f"File is saved at --> {DATA_DIR}")
